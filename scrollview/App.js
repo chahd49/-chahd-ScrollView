b1a9686872ecb0ea1ef4016869d91939539950aa
chahd49 <snxwfqll061707@gmail.com>
2025-05-01 14:16:31 -0400

    Done

--- scrollview/App.js
+++ scrollview/App.js
@@ -1,19 +1,59 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, ScrollView, Text, View } from 'react-native';
 
+
+
 export default function App() {
   return (
-    <ScrollView>
+    
     <View style={styles.container}>
-    <View style={styles.container}>
+    <View style={styles.container1}>
       <Text style={styles.text1}>navbar</Text>
       <StatusBar style="auto" />
     </View>
 
-    <View style={styles.top}>
+    <ScrollView>
+      
+    {/* <View style={styles.top}>
       <Text style={styles.text2}>Image/Video</Text>
       <StatusBar style="auto" />
+    </View> */}
+
+    <View style={styles.root2}>
+          <ScrollView contentContainerStyle={{ height: 1500 }}>
+          <View style={styles.section2}>
+      <Text style={styles.text3}>Section 2</Text>
+      <StatusBar style="auto" />
     </View>
+
+    <View style={styles.section3}>
+      <Text style={styles.text3}>Section 3</Text>
+      <StatusBar style="auto" />
+    </View>
+
+    <View style={styles.section2}>
+      <Text style={styles.text3}>Section 4</Text>
+      <StatusBar style="auto" />
+    </View>
+
+    <View style={styles.section3}>
+      <Text style={styles.text3}>Section 5</Text>
+      <StatusBar style="auto" />
+    </View>
+            <View style={styles.box1}></View>
+            <View style={styles.box2}></View>
+            <View style={styles.box1}></View>
+            <View style={styles.box2}></View>
+            <View style={styles.box1}></View>
+            <View style={styles.box2}></View>
+            <View style={styles.box1}></View>
+            <View style={styles.box2}></View>
+            <View style={styles.box1}></View>
+            <View style={styles.box2}></View>
+            <View style={styles.box1}></View>
+            <View style={styles.box2}></View>
+          </ScrollView>
+        </View>
 
     <View style={styles.section2}>
       <Text style={styles.text3}>Section 2</Text>
@@ -24,12 +64,24 @@ export default function App() {
       <Text style={styles.text3}>Section 3</Text>
       <StatusBar style="auto" />
     </View>
+
+    <View style={styles.section2}>
+      <Text style={styles.text3}>Section 4</Text>
+      <StatusBar style="auto" />
+    </View>
+
+    <View style={styles.section3}>
+      <Text style={styles.text3}>Section 5</Text>
+      <StatusBar style="auto" />
+    </View>
     
+    </ScrollView>
+
     <View style={styles.footer}>
       <Text style={styles.text4}>Footer</Text>
       <StatusBar style="auto" />
     </View>
-    </View></ScrollView>
+    </View>
 
 
   );
@@ -41,41 +93,49 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    height: '100%',
     backgroundColor: 'red',
     alignItems: 'left',
     justifyContent: 'center',
     width: 500
   },
+
+  container1: {
+    height: '10%',
+    backgroundColor: 'red',
+    alignItems: 'left',
+    justifyContent: 'center',
+    width: 500
+  },
+
   top: {
-    flex: 5,
+    height: '250%',
     backgroundColor: 'green',
     alignItems: 'center',
     justifyContent: 'center',
     width: 500
   },
   section2: {
-    flex: 5,
-    backgroundColor: 'blue',
+    height: '50%',
+    backgroundColor: 'black',
     alignItems: 'center',
     justifyContent: 'center',
     width: 500,
-    flex: 5
     
   },
 
   section3: {
-    flex: 5,
+    height: '50%',
     backgroundColor: 'purple',
     alignItems: 'center',
     justifyContent: 'center',
     width: 500,
-    flex: 5
+   
     
   },
 
   footer: {
-    flex: 1,
+    height: '10%',
     backgroundColor: 'orange',
     alignItems: 'center',
     justifyContent: 'center',
@@ -101,5 +161,18 @@ const styles = StyleSheet.create({
 
   text4: {
     color: 'black'
+  },
+
+  root2: {
+    height: '100%',
+    width: '100%', 
+  },
+  box1: {
+    height: 100,
+    backgroundColor: 'blue',
+  },
+  box2: {
+    height: 100,
+    backgroundColor: 'purple',
   },
 });
